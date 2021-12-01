088d1b235db7829c8f9b6e3f6bbfc56c03abef6e
Jan Grulich <jan.grulich@thermofisher.com>
2021-12-01 11:55:29 +0100

    honza: day01

---
+++ src/honza/AdventOfCode/Advent_2021/Day01.cs
@@ -0,0 +1,19 @@
+﻿namespace Advent_2021;
+
+internal class Day01
+{
+    public int SolveFirst()
+    {
+        var measurements = File.ReadAllLines(Path.Combine("Input","Day01.txt")).Select(int.Parse).ToList();
+        return Enumerable.Range(0, measurements.Count - 1).Select(index => measurements[index] < measurements[index + 1]).Count(s => s);
+    }
+
+    public int SolveSecond()
+    {
+        var windowSize = 3;
+        var measurements = File.ReadAllLines(Path.Combine("Input", "Day01.txt")).Select(int.Parse).ToList();
+        var sums = Enumerable.Range(0, measurements.Count - windowSize + 1).Select(index => measurements.Skip(index).Take(windowSize).Sum()).ToList();
+        return Enumerable.Range(0, sums.Count - 1).Select(index => sums[index] < sums[index + 1]).Count(s => s);
+    }
+}
+
---
+++ src/honza/AdventOfCode/Advent_2021/Program.cs
@@ -0,0 +1,7 @@
+﻿using Advent_2021;
+
+var day = new Day01();
+
+Console.WriteLine($"First puzzle: {day.SolveFirst()}");
+
+Console.WriteLine($"Second puzzle: {day.SolveSecond()}");
