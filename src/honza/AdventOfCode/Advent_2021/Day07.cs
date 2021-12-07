9f66bd9e8a666b7916d57a286aa4ece1a8e00125
hgrulich <h.grulich@gmail.com>
2021-12-07 07:07:38 +0100

    honza day07 tweak

--- src/honza/AdventOfCode/Advent_2021/Day07.cs
+++ src/honza/AdventOfCode/Advent_2021/Day07.cs
@@ -11,42 +11,24 @@ namespace Advent_2021
         public int SolveFirst()
         {
             var positions = File.ReadAllLines(Path.Combine("Input", "Day07.txt"))[0].Split(',').Select(int.Parse);
-            var min = positions.Min();
-            var max = positions.Max();
-            var minSum = int.MaxValue;
-            for (int i = min; i <= max; i++)
-            {
-                var sum = positions.Select(x => Math.Abs(x - i)).Sum();
-                if(minSum > sum)
-                    minSum = sum;
-            }
-            return minSum;
+            return this.Solve((crabPos, targetPos) => Math.Abs(crabPos - targetPos));
         }
 
         public int SolveSecond()
         {
             var positions = File.ReadAllLines(Path.Combine("Input", "Day07.txt"))[0].Split(',').Select(int.Parse);
+            return this.Solve((crabPos, targetPos) => MakeIntSum(Math.Abs(crabPos - targetPos)));
+        }
+
+        int Solve(Func<int, int, int> fuelMapping)
+        {
+            var positions = File.ReadAllLines(Path.Combine("Input", "Day07.txt"))[0].Split(',').Select(int.Parse);
             var min = positions.Min();
             var max = positions.Max();
             var minSum = int.MaxValue;
-            for (int i = min; i <= max; i++)
-            {
-                //var sum = positions.Select(x => Enumerable.Range(1, Math.Abs(x - i)).Sum()).Sum(); //3 times slower than private method
-                var sum = positions.Select(x => MakeIntSum(Math.Abs(x - i))).Sum();
-                if (minSum > sum)
-                    minSum = sum;
-            }
-            return minSum;
+            return Enumerable.Range(min, max).Select(i => positions.Select(x => fuelMapping(x, i)).Sum()).Min();
         }
 
-        int MakeIntSum(int value)
-        {
-            var result = 0;
-            for (int i = value; i > 0; i--)
-            {
-                result += i;
-            }
-            return result;
-        }
+        int MakeIntSum(int value) => (value + 1) * value / 2;
     }
 }
